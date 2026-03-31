d86254ce62a2cbb4d0ffb128a7984f9f4f1c3009
Mineral <trevonmacklin1@gmail.com>
2026-03-31 13:45:30 +1100

    actually added the code

---
+++ DATATYPES-VARIABLES.lua
@@ -0,0 +1,24 @@
+--[[
+   nil (nothing)
+   number ex. 0.273 1 200
+   string ex. 'hello' "hello world"
+   boolean (true false)
+   table
+]]
+
+local Loveshot = 10
+
+local name = "John Doe"
+print(Loveshot + 5)
+
+-- ^ this should be 15 
+print("His name is " .. name .. ",  He is over " .. Loveshot .. " years old")
+local evil = true
+
+
+
+name = nil 
+--variable is no longer assigned
+
+Peep = 30
+--global variable
